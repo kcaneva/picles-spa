d13b220b2ddb7d79ce4b2e50f48ce7c3b38772a9
kcaneva <kleber@caneva.com.br>
2024-04-23 22:24:02 -0300

    impl: useRef in Shelter

--- src/pages/Admin/Shelter/Shelter.tsx
+++ src/pages/Admin/Shelter/Shelter.tsx
@@ -1,16 +1,26 @@
+import { useRef } from 'react'
 import { Button } from '../../../components/common/Button'
 import { Input } from '../../../components/common/Input'
 import { Panel } from '../../../components/layout/Panel'
 import styles from './Shelter.module.css'
 
 export function Shelter() {
+  const inputRef = useRef(null);
+
+  function submit(event) {
+    event.preventDefault()
+    console.log(inputRef.current.value)
+  }
+
   return (
     <Panel>
       <form className={styles.container} 
-          // onSubmit={applyFilters}
-          // onChange={checkButtonStatus}
+          onSubmit={submit}
         >
-        <Input name="whatsapp" caption='WhatsApp'/>
+        <Input ref={inputRef} type='text' name="nome" label='Nome'/>        
+        <Input type='email' name="email" label='E-mal'/>
+        <Input type='tel' name="phone" label='Fone'/>
+        <Input type='tel' name="whatsapp" label='WhatsApp'/>
         <Button>
           Salvar dados
         </Button>
